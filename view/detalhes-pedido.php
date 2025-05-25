e7b6cd3b7797eb00455335f4fa0d120ee088969f
Duda Souza <dudaesouza@gmail.com>
2025-05-25 18:27:31 -0300

    arrumado rolagem de detalhes

--- view/detalhes-pedido.php
+++ view/detalhes-pedido.php
@@ -49,6 +49,12 @@ try {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="dashboard.css">
+    <style>
+        body {
+            overflow-y: auto; 
+            height: auto; 
+        }
+    </style>
 </head>
 <body>
     <div class="header">
